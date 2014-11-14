ba63cda51804b08d8592872dc8728c0f99306b26
dcuddhy <dcuddhy@gmail.com>
2014-11-14 11:54:38 -0700

    email presence required

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ActiveRecord::Base
   has_secure_password
 
-  validates :email, uniqueness: {case_sensitive: false}
+  validates :email, presence: true, uniqueness: {case_sensitive: false}
   validates :name, presence: true, uniqueness: {case_sensitive: false}
+
 
   before_validation do
     self.email = email? ? email.downcase : nil
